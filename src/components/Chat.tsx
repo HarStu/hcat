a5c370d7d1134bdcb2f8e256b0793f7ab7dbe19b
Harrison <33430956+HarStu@users.noreply.github.com>
2025-06-09 13:27:39 -0400

    Improve message-generating styling

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,15 +1,19 @@
 'use client'
 import { useChat } from 'ai/react'
 import { Spinner } from './ui/spinner'
+import clsx from 'clsx'
 
 export function Chat() {
-  const { messages, setMessages, input, handleInputChange, handleSubmit, status, stop, error, reload } = useChat()
+  const { messages, input, handleInputChange, handleSubmit, status, stop, error, reload } = useChat()
 
-  const buttonClass = 'flex px-2 border rounded items-center'
+  const buttonClass = 'flex px-2 min-w-16 border rounded items-center justify-center'
+  const messagesContainerClass = 'flex-1 border rounded p-4 mb-0 overflow-y-auto transition-all duration-900 ease-in-out'
+
+  const generating = (status === 'submitted' || status === 'streaming')
 
   return (
     <div className="flex flex-col w-full max-w-md mx-auto h-screen">
-      <div className="flex-1 overflow-y-auto">
+      <div className={clsx(messagesContainerClass)}>
         {messages.map(m => (
           <div key={m.id} className="mb-4">
             <strong>{m.role}: </strong>
@@ -18,26 +22,24 @@ export function Chat() {
         ))}
       </div>
 
-      {(status === 'submitted' || status === 'streaming') && (
-        <div>
-          <button type='button' className={buttonClass} onClick={() => stop()}>
-            cancel
-          </button>
-        </div>
-      )}
+      <div className={clsx('flex items-center transition-all duration-300 ease-in-out h-4', generating ? 'opacity-100 h-8 p-1' : 'opacity-0')} >
+        <Spinner />
+      </div>
 
-      {error && (
-        <div>
+      {
+        error && (
           <div>
-            error communicating with AI
-          </div>
-          <button type='button' className={buttonClass} onClick={() => reload()}>
-            retry
-          </button>
-        </div>
-      )}
+            <div>
+              error communicating with AI
+            </div>
+            <button type='button' className={buttonClass} onClick={() => reload()}>
+              retry
+            </button>
+          </div >
+        )
+      }
 
-      <form onSubmit={handleSubmit} className="flex gap-2 py-8 justify-end">
+      <form onSubmit={handleSubmit} className="flex gap-2 pb-4 justify-end">
         <input
           value={input}
           onChange={handleInputChange}
@@ -46,8 +48,8 @@ export function Chat() {
           disabled={error != null}
         />
         {(status === 'ready') ?
-          (<button type='submit' className={buttonClass}>Send</button>) :
-          (<Spinner />)
+          (<button type='submit' className={buttonClass}>send</button>) :
+          (<button type='button' className={buttonClass} onClick={() => stop()}>cancel</button>)
         }
 
       </form>
